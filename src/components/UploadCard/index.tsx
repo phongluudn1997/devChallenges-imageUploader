539a7ddad901ab2a33ec264fdb2051b641ec621f
Phong Luu <huynhpluu@gmail.com>
2020-10-06 23:54:00 +0700

    add html and css for UI

--- src/components/UploadCard/index.tsx
+++ src/components/UploadCard/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import style from "./style.module.scss";
+import placeholder from "../../assets/imgs/placeholder.png";
 
 const UploadCard = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className={style["card"]}>
+      <h3 className={style["card__title"]}>Upload your image</h3>
+      <p className={style["card__description"]}>File should be Jpeg, Png...</p>
+      <div className={style["card__drop-area"]}>
+        <img src={placeholder} alt="drop area" />
+        <p>Drag & Drop you image here</p>
+      </div>
+      <p className={style["card__divider"]}>Or</p>
+      <div>
+        <button className={style["card__input"]}>Choose a file</button>
+      </div>
+    </div>
+  );
 };
 
 export default UploadCard;

b9c6896dbf7c831325f1601feb63159f58a636f5
Phong Luu <luu.huynh@fiisoft.com>
2020-10-07 14:57:30 +0700

    get files before uploading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
+import "./App.css";
 import UploadCard from "./components/UploadCard";
 
+enum StatusEnum {
+  IDLE,
+  PENDING,
+  RESOVED,
+  REJECT,
+}
+
 function App() {
+  const [status, setStatus] = useState<StatusEnum>(StatusEnum.IDLE);
+
+  const handleFiles = (files: FileList): void => {
+    const validFiles = Object.values(files).filter((file) => {
+      return isFileValid(file);
+    });
+    previewAndUpload(validFiles);
+  };
+
+  const isFileValid = (file: File): boolean => {
+    const validTypes = ["image/jpeg", "image/png", "image/gif"];
+    const maxSizeInBytes = 10e6;
+    return validTypes.indexOf(file.type) !== -1 && file.size <= maxSizeInBytes;
+  };
+
+  const previewAndUpload = (files: Array<File>): void => {
+    setStatus(StatusEnum.PENDING);
+    axios
+      .post("http://localhost:8000", files)
+      .then((response) => {
+        setStatus(StatusEnum.RESOVED);
+        console.log(response);
+      })
+      .catch((error) => {
+        setStatus(StatusEnum.REJECT);
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <UploadCard />
+      <UploadCard handleFiles={handleFiles} />
     </div>
   );
 }
--- src/components/UploadCard/index.tsx
+++ src/components/UploadCard/index.tsx
@@ -2,18 +2,43 @@ import React from "react";
 import style from "./style.module.scss";
 import placeholder from "../../assets/imgs/placeholder.png";
 
-const UploadCard = () => {
+const UploadCard = (props: any) => {
+  const handleDrop = (e: any) => {
+    const data = e.dataTransfer;
+    const { files } = data;
+    props.handleFiles(files);
+  };
+
+  const preventDefault = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
   return (
     <div className={style["card"]}>
       <h3 className={style["card__title"]}>Upload your image</h3>
       <p className={style["card__description"]}>File should be Jpeg, Png...</p>
-      <div className={style["card__drop-area"]}>
+      <div
+        className={style["card__drop-area"]}
+        onDragEnter={(e) => preventDefault(e)}
+        onDragOver={(e) => preventDefault(e)}
+        onDragEnd={(e) => preventDefault(e)}
+        onDrop={(e) => {
+          preventDefault(e);
+          handleDrop(e);
+        }}
+      >
         <img src={placeholder} alt="drop area" />
         <p>Drag & Drop you image here</p>
       </div>
       <p className={style["card__divider"]}>Or</p>
       <div>
-        <button className={style["card__input"]}>Choose a file</button>
+        <input
+          onChange={(e) => props.handleFiles(e.target.files)}
+          multiple={true}
+          type="file"
+          className={style["card__input"]}
+        ></input>
       </div>
     </div>
   );

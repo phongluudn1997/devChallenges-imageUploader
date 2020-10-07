28d616e22a27afae47f138bb0136c9f8883317f4
Phong Luu <luu.huynh@fiisoft.com>
2020-10-07 16:24:20 +0700

    added: backend handling for uploading images

--- Back End/index.js
+++ Back End/index.js
@@ -1,14 +1,45 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const port = 3001;
+const multer = require("multer");
+const cors = require("cors");
+const port = 8000;
 
-app.get('/', (req, res) => {
+app.use(cors());
+app.use(express.static("public"));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage,
+}).array("files");
+
+app.post("/upload", (req, res) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).json(err);
+    } else {
+      res.status(500).json(err);
+    }
     res.status(200).json({
-        sucess: true,
-        data: "Yay! Connect successfully!"
-    })
-})
+      files: req.files,
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    data: "Yay! Connect successfully!",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
